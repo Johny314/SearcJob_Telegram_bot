3b9f02097442e095f88a9941c30b8d9a067c5fea
Johny314 <Jekazombi@gmail.com>
2024-12-26 16:57:32 +0500

    S009 | upgrade skill_list

--- services/analyze_service.py
+++ services/analyze_service.py
@@ -3,12 +3,67 @@ from collections import Counter
 
 # Список заранее определенных навыков (можно будет вынести его в файл конфигурации)
 SKILLS_LIST = [
-    'python', 'java', 'javascript', 'sql', 'c++', 'html',
-    'css', 'django', 'fastapi', 'git', 'react', 'docker',
-    'kubernetes', 'aws', 'azure', 'devops', 'node.js',
-    'graphql', 'mongo', 'mysql', 'postgresql', 'linux',
-    'tensorflow', 'keras', 'flutter', 'typescript', 'nosql',
-    'machine learning', 'data analysis'
+    # Языки программирования
+    'python', 'java', 'javascript', 'typescript', 'c', 'c++', 'c#', 'go',
+    'ruby', 'php', 'swift', 'kotlin', 'r', 'perl', 'dart', 'scala', 'rust',
+    'objective-c', 'matlab', 'shell', 'powershell', 'visual basic',
+    'assembly', 'lua',
+
+    # Веб-разработка: фронтенд
+    'html', 'css', 'bootstrap', 'sass', 'less', 'javascript', 'jquery',
+    'react', 'vue.js', 'angular', 'svelte', 'next.js', 'nuxt.js', 'elm',
+
+    # Веб-разработка: бэкенд
+    'node.js', 'express.js', 'nest.js', 'django', 'flask', 'fastapi',
+    'spring', 'spring boot', 'asp.net', 'laravel', 'symfony',
+    'rails', 'ruby on rails', 'cakephp', 'gin',
+
+    # Мобильная разработка
+    'flutter', 'react native', 'xamarin', 'jetpack compose', 'kotlin multi-platform',
+
+    # Работа с данными
+    'sql', 'mysql', 'postgresql', 'sqlite', 'mongodb', 'redis', 'couchdb',
+    'cassandra', 'hive', 'bigquery', 'clickhouse', 'elasticsearch',
+    'spark', 'hadoop', 'presto', 'dask', 'pandas', 'numpy',
+
+    # DevOps и системное администрирование
+    'docker', 'kubernetes', 'jenkins', 'terraform', 'ansible', 'puppet',
+    'saltstack', 'vagrant', 'nginx', 'apache', 'haproxy', 'prometheus',
+    'grafana', 'elastic stack', 'splunk', 'bash', 'zabbix',
+
+    # Облачные платформы
+    'aws', 'amazon web services', 'azure', 'google cloud platform',
+    'ibm cloud', 'oracle cloud', 'digitalocean', 'heroku', 'vercel',
+
+    # Машинное обучение и Data Science
+    'tensorflow', 'keras', 'pytorch', 'scikit-learn', 'xgboost',
+    'lightgbm', 'mlflow', 'data analysis', 'machine learning',
+    'deep learning', 'computer vision', 'natural language processing',
+    'openai', 'huggingface',
+
+    # Работа с графикой и анимацией
+    'three.js', 'unity', 'unreal engine', 'blender',
+
+    # Работа с API и микро-сервисами
+    'graphql', 'rest', 'soap', 'grpc', 'json',
+
+    # Инструменты тестирования
+    'selenium', 'cypress', 'pytest', 'junit', 'mocha', 'karma',
+    'jasmine', 'appium', 'testng',
+
+    # Виртуализация и контейнеризация
+    'vmware', 'hyper-v', 'qemu', 'virtualbox',
+
+    # Безопасность
+    'penetration testing', 'ethical hacking', 'kali linux',
+    'nessus', 'nmap',
+
+    # Дополнительно
+    'git', 'github', 'gitlab', 'bitbucket', 'ci/cd', 'linux', 'shell scripting',
+    'postgresql', 'firebase', 'stripe', 'mapbox', 'openstreetmap',
+
+    # Ручное и автоматическое тестирование
+    'postman', 'jmeter', 'soapui', 'katalon studio',
 ]
 
 
